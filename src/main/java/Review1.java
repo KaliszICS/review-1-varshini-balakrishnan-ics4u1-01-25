d14771611224e6beff8f431410e9c920d7a84208
varshini-balakrishnan-ics4u1-01-25 <varshini.balakrishnan@student.tdsb.on.ca>
2025-09-08 18:26:11 +0000

    update Review1.java

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,23 @@
 public class Review1 {
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+	public static void main(String[]args) {
+	} {
+		//q1
+		System.out.println("There once was a man from St. Ives");
+		//q2
+		int n = 9;
+		int f = 5;
+		System.out.println(n / f);
+		System.out.println(n % f);
 
-	public static void q2() {
-		//Write question 2 code here
+		//q3
+		System.out.println("Hello\nHello again");
+		//q4
+		System.out.println(true);
+		//q5
+		String one = "Hello";
+		String two = "world";
+		System.out.println(one + " ");
+		System.out.println(two);
 	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
-}
+]
